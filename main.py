e26e57f9f872b005914dd51487446a80182b0a1b
vlasik <vlasiigolovin@gmail.com>
2024-05-18 15:53:51 +0200

    upd

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ class Controller(Entity):
         super().__init__(ignore_paused = True, **kwargs)
         self.sky = Sky(texture='sky_sunset')
         self.ground = Entity(model='plane', collider='box', scale=64, texture='grass', texture_scale=(4,4))
+        self.backrooms = Entity(model="assets/backrooms_cubes_room/scene", parents=scene, scale=2, collider="mesh", origin_y=-0.5)
         self.player = FirstPersonController()
         self.player.gun = Entity(model='assets/thompson_submachine_gun/scene', 
-                                 parent=camera, position=(.5,-.25,.25), scale=(.3,.2,1),
-                                origin_z=-.5, rotation=(0,0,0), on_cooldown=False)
+                                 parent=camera, position =Vec3(0.352125, -0.219659, 0.445983), scale=(.3,.3,.3),
+                                origin_z=-.5, rotation=Vec3(0.144472, -81.176, -353.45), on_cooldown=False)
 
-
-
+        
 
 window.title = "shoot and run"
 window.fullscreen = True
