c4a7fbb4168dbcb9929abd0ed0b1014ada362c86
vlasik <vlasiigolovin@gmail.com>
2024-05-23 17:45:36 +0200

    upd

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 from ursina.prefabs.sky import Sky
 from ursina.shaders import lit_with_shadows_shader
 
+from models import Backrooms, Player, Partygoer, Bacteria
+
 app = Ursina()
 Entity.default_shader = lit_with_shadows_shader
 
@@ -11,14 +13,11 @@ class Controller(Entity):
         super().__init__(ignore_paused = True, **kwargs)
         #self.sky = Sky(texture='sky_sunset')
         self.ground = Entity(model='plane', collider='box', scale=64, texture='grass', texture_scale=(4,4))
-        self.backrooms = Entity(model="assets\level\scene.gltf", parents=scene, scale=0.1, collider="box", origin_y=0)
-        self.player = FirstPersonController()
-        self.enemy = Entity(model="assets/bacteria_backrooms/scene", parent=scene, scale=0.7, collider="mesh", origin_y=0, color=color.black)
-        self.enemy2 = Entity(model="assets\partygoer_from_backrooms\scene.gltf", parent=scene, scale=2.2, collider="mesh", origin_y=0)
+        self.backrooms = Backrooms()
+        self.player = Player(self)  
+        self.enemy = Bacteria()
+        self.enemy2 = Partygoer()
         self.enemy2.x = 5
-        self.player.gun = Entity(model='assets/thompson_submachine_gun/scene', 
-                                 parent=camera, position =Vec3(0.352125, -0.219659, 0.445983), scale=(.3,.3,.3),
-                                origin_z=-.5, rotation=Vec3(0.144472, -81.176, -353.45), on_cooldown=False)
         
 
 
---
+++ models.py
@@ -0,0 +1,54 @@
+
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+class Gun(Entity):
+    def __init__(self, ):
+        super().__init__(model='assets/thompson_submachine_gun/scene', 
+                                 parent=camera, position =Vec3(0.352125, -0.219659, 0.445983), scale=(.3,.3,.3),
+                                origin_z=-.5, rotation=Vec3(0.144472, -81.176, -353.45), on_cooldown=False)
+
+class Player(FirstPersonController):
+    
+    def __init__(self, game):
+        super().__init__()
+        self.gun = Gun()
+        self.game = game
+    
+    def input(self, key):
+        super().input(key)
+                 
+
+    def check_collisions(self):
+        forward = self.forward * self.speed * time.dt
+        hit_check = raycast(self.position, self.forward, distance = self.speed*time.dt, ignore=[self])
+        if hit_check.hit:
+            print("Collision!!!", hit_check.entity)
+
+    
+    def update(self):
+        super().update()
+        if held_keys["shift"]:
+            self.speed = 10
+        else:
+            self.speed = 5
+
+        if self.y < -30:
+            self.position = self.start_pos
+
+        self.check_collisions()
+
+class Backrooms(Entity):
+    def __init__(self, ):
+        super().__init__(model="assets\original_backrooms\scene.gltf", parents=scene, scale=2, collider="box", origin_y=0)
+
+        self.collider = self.model
+
+class Bacteria(Entity):
+    def __init__(self):
+        super().__init__(model="assets/bacteria_backrooms/scene", parent=scene, scale=0.7, collider="mesh", origin_y=0, color=color.black)
+
+
+class Partygoer(Entity):
+    def __init__(self):
+        super().__init__(model="assets/partygoer_from_backrooms/scene.gltf", parent=scene, scale=2.2, collider="mesh", origin_y=0)
